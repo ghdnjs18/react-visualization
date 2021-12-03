cd5aebe80de17d1688c98183216fc111ecf0db99
ghdnjs18 <ghdnjs18@naver.com>
2021-12-03 12:19:38 +0900

    CRA 로 Chartjs 프로젝트 init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+// import { Chart, registerables } from "chart.js";
+// Chart.register(...registerables);
+import Chart from "chart.js/auto";
 
 function App() {
+  console.log();
+  const canvasDom = useRef(null);
+
+  useEffect(() => {
+    const ctx = canvasDom.current.getContext("2d");
+    const barChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        datasets: [
+          {
+            data: [1, 5],
+          },
+        ],
+      },
+    });
+    return () => {
+      barChart.destroy();
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello React
+      <canvas ref={canvasDom} />
     </div>
   );
 }
